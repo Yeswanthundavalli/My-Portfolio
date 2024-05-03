c568b3d28be0a32a14ca154dc7d841a53522cd51
Yeswanthundavalli <yeswanthundavalli1@gmail.coom>
2024-05-03 09:12:01 +0530

    first commit

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
-function opentab(tabname){
-    for(tablink of tablinks){
+function opentab(event, tabname){
+    for(let tablink of tablinks){
         tablink.classList.remove("active-link");
     }
 
-    for(tabcontent of tabcontents){
+    for(let tabcontent of tabcontents){
         tabcontent.classList.remove("active-tab");
     }
 
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
-
-
